8228f1914401eef7c5021409151ef31a6517535c
WINDOWS-M8A9A01\User <soltanovskiy@brights.com.ua>
2019-01-08 00:38:17 +0200

    Desktop: promo form markup and styles, targets hd bg

---
+++ src/js/components/forms.js
@@ -0,0 +1,6 @@
+const promoForm = document.querySelector('.js-promo__form');
+const promoSubmitHandler = function(e){
+    e.preventDefault();
+}
+
+promoForm.addEventListener('submit', promoSubmitHandler)
--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@ import "./partials/helpers";
 import header from "@comp/header";
 import toggleClients from "@comp/clients";
 import sliderResponces from "@comp/responces";
+import "@comp/forms";
