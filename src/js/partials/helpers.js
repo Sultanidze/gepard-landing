abde6545a78b5d9a7b190566d082d092d224dfdd
WINDOWS-M8A9A01\User <soltanovskiy@brights.com.ua>
2019-01-09 23:11:12 +0200

    Added new clients logos, added forEach polyfill, smooth scroll to anchors under header, html validation fixes

--- src/js/partials/helpers.js
+++ src/js/partials/helpers.js
@@ -1,6 +1,17 @@
+import 'nodelist-foreach-polyfill';
 import '@iconfu/svg-inject';
 import objectFitImages from 'object-fit-images';
+import jump from 'jump.js'
 
 SVGInject(document.querySelectorAll("img.svg_injectable"));
 
 objectFitImages();
+
+const anchorLinks = document.querySelectorAll('.js-link_anchor');
+const anchorLinkClickHandler = function(e) {
+    e.preventDefault();
+    
+    const href = this.hash;
+    jump(href);
+};
+anchorLinks.forEach(link => link.addEventListener('click', anchorLinkClickHandler))
