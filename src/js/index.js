bb0a9d3dcf29d4d1660f8adbde0eac246a0fb132
Sultanidze <Sultanidze@ukr.net>
2019-02-11 02:22:14 +0200

    About page: desktop beta done

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "@babel/polyfill";
 import "./partials/helpers";
 import "@comp/clients";
+import "@comp/team";
 import sliderResponces from "@comp/sliders/responces";
 import sliderAbout from "@comp/sliders/about";
 import "@comp/forms/";
